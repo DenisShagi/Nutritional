656a73c0113e767685859324ca95ae65af7e9222
Denis <denis.shagim@icloud.com>
2025-01-20 13:46:34 +0300

    init: Customizing postcss, tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1>Start</h1>
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
     </>
   );
 }
